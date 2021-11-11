50ad8858cb919d3e6c88f0c397cf497e711a859b
Edd Robinson <me@edd.io>
2021-11-11 11:40:56 +0000

    chore: ensure system protoc used

--- scripts/build_iox.sh
+++ scripts/build_iox.sh
@@ -6,9 +6,15 @@ set -x
 CLONE_DIR=/tmp/iox
 CARGO_PATH=$HOME/.cargo/bin/cargo 
 
+# Required to use `arm64` system protoc 
+PROTOC=/opt/homebrew/bin/protoc 
+PROTOC_INCLUDE=/opt/homebrew/include
+
 echo "Checking build dependencies..."
 command -v clang >/dev/null || (echo "Installing clang..." && brew update && brew install clang)
 command -v $CARGO_PATH >/dev/null || (echo "Rust needs to be installed" && exit 1)
+command -v protoc >/dev/null || (echo "Installing protoc via protobuf package" && brew update && brew install protobuf)
+
 
 [ -d $CLONE_DIR ] && echo "Cleaning $CLONE_DIR" && rm -rf $CLONE_DIR
 echo "Creating $CLONE_DIR"
